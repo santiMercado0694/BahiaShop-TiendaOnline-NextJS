4eea48c9128546a1b7a920ac8d51ee156982aafa
Lucas González <lucasgxnzalez@gmail.com>
2024-06-20 21:25:10 -0300

    MP completamente funcional

--- bahiashop/src/app/layout.tsx
+++ bahiashop/src/app/layout.tsx
@@ -21,24 +21,25 @@ export default function RootLayout({
 }>) {
   return (
     <SessionWrapper>
-      <AppProvider>
-        <html lang="en" className="h-full">
-          <body 
-            className={cn(
-              "relative h-full font-sans antialiased",
-              inter.className
-            )}>
-            <main className="relative flex flex-col min-h-screen"> 
-              <Navbar/>        
-              <div className="flex-grow flex-1">
-              <div className="mb-8"></div> 
-                {children}
-              </div>
-              <Footer/>
-            </main>
-          </body>
-        </html>
-      </AppProvider>
+        <AppProvider>
+          <html lang="en" className="h-full">
+            <body
+              className={cn(
+                "relative h-full font-sans antialiased",
+                inter.className
+              )}
+            >
+              <main className="relative flex flex-col min-h-screen">
+                <Navbar />
+                <div className="flex-grow flex-1">
+                  <div className="mb-8"></div>
+                  {children}
+                </div>
+                <Footer />
+              </main>
+            </body>
+          </html>
+        </AppProvider>
     </SessionWrapper>
   );
 }
